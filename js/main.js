e01780cee165051edbfa859e134697c0118488ca
muzamil <muzamil-javascriptdev@outlook.com>
2021-08-05 18:49:43 +0500

    mobile-responive

--- js/main.js
+++ js/main.js
@@ -1,9 +1,49 @@
 $(document).ready(function () {
-  $(".owl-carousel").owlCarousel({
-    items: 1,
+  $(".owl-carousel.img-carousel").owlCarousel({
+    // items: 1,
     margin: 14,
-    video: true,
     lazyLoad: true,
-    autoWidth: true,
+    // autoWidth: true,
+    center: false,
+    nav: true,
+    dots: false,
+    navText: [
+      "<div class='nav-btn prev-slide'></div>",
+      "<div class='nav-btn next-slide'></div>",
+    ],
+    responsive: {
+      0: {
+        items: 3,
+      },
+      350: {
+        items: 6,
+      },
+    },
+  });
+  $(".owl-carousel.video").owlCarousel({
+    items: 1,
+    merge: false,
+    loop: false,
+    margin: 10,
+    video: true,
+    dots: false,
+    nav: true,
+    navText: [
+      "<div class='nav-btn--video prev-slide--video'></div>",
+      "<div class='nav-btn--video next-slide--video'></div>",
+    ],
+    lazyLoad: false,
+    video: true,
+    videoWidth: 300,
+
+    center: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      992: {
+        items: 3,
+      },
+    },
   });
 });
